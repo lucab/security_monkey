db0e9de55f0802b038b1d72978194d90a21a5da7
Luca Bruno <lucab@debian.org>
2015-02-10 17:19:10 +0100

    sg: link groups to related instances
    Signed-off-by: Luca Bruno <luca.bruno@rocket-internet.de>

--- security_monkey/watchers/security_group.py
+++ security_monkey/watchers/security_group.py
@@ -62,8 +62,13 @@ class SecurityGroup(Watcher):
 
                 try:
                     rec2 = connect(account, 'ec2', region=region)
+                    # Retrieve security groups here
                     sgs = self.wrap_aws_rate_limited_call(
                         rec2.get_all_security_groups
+                    )
+                    # We fetch tags here to later correlate instances
+                    tags = self.wrap_aws_rate_limited_call(
+                        rec2.get_all_tags
                     )
                 except Exception as e:
                     if region.name not in TROUBLE_REGIONS:
@@ -72,6 +77,7 @@ class SecurityGroup(Watcher):
                     continue
 
                 app.logger.debug("Found {} {}".format(len(sgs), self.i_am_plural))
+
                 for sg in sgs:
 
                     if self.check_ignore_list(sg.name):
@@ -84,8 +90,10 @@ class SecurityGroup(Watcher):
                         "vpc_id": sg.vpc_id,
                         "owner_id": sg.owner_id,
                         "region": sg.region.name,
-                        "rules": []
+                        "rules": [],
+                        "assigned_to": None
                     }
+
                     for rule in sg.rules:
                         for grant in rule.grants:
                             rule_config = {
@@ -99,6 +107,22 @@ class SecurityGroup(Watcher):
                             }
                             item_config['rules'].append(rule_config)
                     item_config['rules'] = sorted(item_config['rules'])
+
+                    # Retrieve all instances using this sg
+                    sg_filter = {"instance.group-id" : sg.id}
+                    instances = self.wrap_aws_rate_limited_call(rec2.get_only_instances, filters=sg_filter)
+
+                    # Correlate instances with their name tags
+                    assign_inst = []
+                    for i in instances:
+                        inst_tags = [x.value for x in tags if x.name == "Name" and x.res_id == i.id]
+                        (inst_name,) = inst_tags
+                        if inst_name:
+                            assign_inst.append("%s (%s)" % (i.id, inst_name))
+                        else:
+                            assign_inst.append("%s" % i.id)
+                    if assign_inst:
+                        item_config["assigned_to"] = assign_inst
 
                     # Issue 40: Security Groups can have a name collision between EC2 and
                     # VPC or between different VPCs within a given region.
